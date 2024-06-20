84ea73d87c52fcee067e66dff4a03a8852c0f28a
Vittorio Romeo <mail@vittorioromeo.com>
2024-06-20 12:26:51 +0200

    x

---
+++ examples/text_benchmark/TextBenchmark.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/RenderTexture.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+#include <cstdlib>
+
+int main()
+{
+    const auto       font         = sf::Font::loadFromFile("resources/tuffy.ttf").value();
+    const sf::String textContents = "abcdefghilmnopqrstuvz\nabcdefghilmnopqrstuvz\nabcdefghilmnopqrstuvz\n";
+
+    auto text          = sf::Text(font, textContents);
+    auto renderTexture = sf::RenderTexture::create({1680, 1050}).value();
+
+    for (std::size_t i = 0; i < 10'000; ++i)
+    {
+        text.setOutlineThickness(5 + (i % 2));
+        renderTexture.draw(text);
+    }
+}
