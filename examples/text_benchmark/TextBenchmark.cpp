423de8c6485fc7b86ce85136d58e48c14998acd1
Vittorio Romeo <mail@vittorioromeo.com>
2024-06-20 21:13:42 +0200

    wip

--- examples/text_benchmark/TextBenchmark.cpp
+++ examples/text_benchmark/TextBenchmark.cpp
@@ -12,9 +12,13 @@ int main()
     auto text          = sf::Text(font, textContents);
     auto renderTexture = sf::RenderTexture::create({1680, 1050}).value();
 
+    renderTexture.clear();
+
     for (std::size_t i = 0; i < 100'000; ++i)
     {
         text.setOutlineThickness(5 + (i % 2));
         renderTexture.draw(text);
     }
+    
+    renderTexture.display();
 }
